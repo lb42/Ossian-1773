e51743b71f885d96f757d60db52d137edc69f02e
lb42 <lou.burnard@retired.ox.ac.uk>
2021-03-18 22:59:09 +0000

    more tweaks

--- Scripts/reStream.sh
+++ Scripts/reStream.sh
@@ -2,6 +2,7 @@
 # run this from Scripts directory
 cd ../V1/txt
 perl ../../Scripts/streamer.prl ../../Scripts/V1-files.txt
+#fix croma
 cd ../../V2/txt
 perl ../../Scripts/streamer.prl ../../Scripts/V2-files.txt
  

8ee594582e70fc25060e90229438fe0046938d3f
lou burnard <lou.burnard@retired.ox.ac.uk>
2021-03-21 22:05:51 +0000

    first tei version

--- Scripts/tidyUp.sh
+++ Scripts/tidyUp.sh
@@ -1,3 +1,5 @@
-cd ../V1/xml
+#cd ../V1/xml
+#for f in *.xml; do echo $f; saxon $f ../../Scripts/tidyUp.xsl > ../tei/$f; done
+cd ../V2/xml
 for f in *.xml; do echo $f; saxon $f ../../Scripts/tidyUp.xsl > ../tei/$f; done
 	 

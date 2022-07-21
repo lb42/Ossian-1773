15b72c8ceba3e01041afefedc9ddbd9a61d489d7
lb42 <lou.burnard@retired.ox.ac.uk>
2022-07-21 11:56:09 +0100

    add NUIG HTML version

--- Scripts/ossianUp.sh
+++ Scripts/ossianUp.sh
@@ -1,7 +1,9 @@
-saxon -xi driver-vol1.tei Scripts/ossianUp.xsl >ossianedition6a.xml
-xsltproc Scripts/NUIG/myDefault.xsl ossianedition6a.xml > ed6.html
+#change next line if local copy is elsewhere
+cd /home/lou/Public/Ossian-1773/NUIG
+saxon -xi driver-vol1.tei ossianUp.xsl >ossianedition6a.xml
+xsltproc NUIG/myDefault.xsl ossianedition6a.xml > ed6.html
 cat HTML-prefix.htm ed6.html HTML-suffix.htm > ossianedition6a.html
 rm ed6.html
-saxon -xi driver-vol2.tei Scripts/ossianUp.xsl >ossianedition6b.xml
-xsltproc Scripts/NUIG/myDefault.xsl ossianedition6b.xml > ed6.html
+saxon -xi driver-vol2.tei ossianUp.xsl >ossianedition6b.xml
+xsltproc NUIG/myDefault.xsl ossianedition6b.xml > ed6.html
 cat HTML-prefix.htm ed6.html HTML-suffix.htm > ossianedition6b.html
